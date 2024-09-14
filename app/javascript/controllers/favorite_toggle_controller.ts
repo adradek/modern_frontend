fc09b43979165a1f78ef554ca22a2acf9c590626
Alex Kochurov <alex.kochurov@gmail.com>
2024-09-14 12:57:05 +0300

    Implement the logic using Stimulus values and classes

--- app/javascript/controllers/favorite_toggle_controller.ts
+++ app/javascript/controllers/favorite_toggle_controller.ts
@@ -1,8 +1,38 @@
-import { Controller } from "@hotwired/stimulus";Controller
+import { Controller } from "@hotwired/stimulus"
 
 export default class FavoriteToggleController extends Controller {
-  toggle(event): void {
-    console.log(event.params);
-    console.log(event.params.text);
+  static classes = ["hidden"]
+  hiddenClass: string
+
+  static targets = ["elementToHide", "elementWithText"]
+  elementToHideTarget: HTMLElement
+  elementWithTextTarget: HTMLElement
+
+  static values = { visible: Boolean }
+  visibleValue: boolean
+
+  toggle(): void {
+    this.flipState()
+  }
+
+  flipState(): void {
+    this.visibleValue = !this.visibleValue
+  }
+
+  visibleValueChanged(): void {
+    this.updateHiddenClass()
+    this.updateText()
+  }
+
+  updateHiddenClass(): void {
+    this.elementToHideTarget.classList.toggle(this.hiddenClass, !this.visibleValue)
+  }
+
+  newText(): string {
+    return this.visibleValue ? "Hide" : "Show"
+  }
+
+  updateText(): void {
+    this.elementWithTextTarget.innerText = this.newText()
   }
 }
