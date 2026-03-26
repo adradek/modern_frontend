a0542881e4e6891a519f229ee9c68dc0a47a9045
Alex Kochurov <alex.kochurov@gmail.com>
2026-03-26 13:12:17 +0300

    Chapter 2 - Turbo Tag + Turbo Stream

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -24,16 +24,14 @@
 class Concert < ApplicationRecord
   belongs_to :venue
 
-  has_many :gigs,
-    -> { order(order: :asc) },
-    dependent: :destroy,
-    inverse_of: :concert
-
+  has_many :gigs, -> { order(order: :asc) }, dependent: :destroy, inverse_of: :concert
   has_many :bands, through: :gigs
   has_many :tickets, dependent: :destroy
 
-  enum ilk: {concert: "concert", meet_n_greet: "meet_n_greet", battle: "battle"}
-  enum access: {general: "general", members: "members", vips: "vips"}
+  validates :name, presence: true
+
+  enum ilk: { concert: "concert", meet_n_greet: "meet_n_greet", battle: "battle" }
+  enum access: { general: "general", members: "members", vips: "vips" }
 
   def start_day
     start_time.to_date
